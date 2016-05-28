1fb316fc4f3e109ad358daf83895d1ce29012640
jake <jirhiker@gmail.com>
2016-05-28 03:01:11 -0600

    updated app. added smoothing

--- app.py
+++ app.py
@@ -19,12 +19,14 @@ from chaco.chaco_plot_editor import ChacoPlotItem
 from pyface.timer.timer import Timer
 from traits.api import HasTraits, Int, Float, Instance, Array, Enum, Bool, Button, Str, List
 from traitsui.api import View, Item, HGroup, Handler, spring, Group, UItem, EnumEditor
+from enable.api import ComponentEditor
+from chaco.api import Plot, ArrayPlotData
 
 # ============= standard library imports ========================
 import glob
 import time
 import os
-from numpy import hstack
+from numpy import hstack, r_, ones, convolve, asarray
 
 # ============= local library imports  ==========================
 
@@ -32,35 +34,50 @@ from numpy import hstack
 from paths import unique_path
 from sensor import LightSensor
 
-PERIOD = 1000
+PERIOD = 250
+
+def smooth(x, window_len=6, window='hanning'):
+    """
+    avaliable windows=['flat', 'hanning', 'hamming', 'bartlett', 'blackman']
+
+    """
+    x = asarray(x)
+    s = r_[2 * x[0] - x[window_len - 1::-1], x, 2 * x[-1] - x[-1:-window_len:-1]]
+
+    if window == 'flat':  # moving average
+        w = ones(window_len, 'd')
+    else:
+        mod = __import__('numpy', fromlist=[window])
+        func = getattr(mod, window)
+        w = func(window_len)
+
+    y = convolve(w / w.sum(), s, mode='same')
+    return y[window_len:-window_len + 1]
 
 
 class Viewer(HasTraits):
-    """ This class just contains the two data arrays that will be updated
-    by the Controller.  The visualization/editor for this class is a
-    Chaco plot.
     """
-    index = Array
+    """
 
-    data = Array
+    def __init__(self, *args, **kw):
+        super(Viewer, self).__init__(*args, **kw)
 
-    view = View(ChacoPlotItem("index", "data",
-                              # type_trait="plot_type",
-                              resizable=True,
-                              x_label="Time",
-                              y_label="Signal",
-                              color="blue",
-                              bgcolor="white",
-                              border_visible=True,
-                              border_width=1,
-                              padding_bg_color="lightgray",
-                              width=800,
-                              height=380,
-                              marker_size=2,
-                              show_label=False),
-                resizable=True,
-                buttons=["OK"],
-                width=800, height=500)
+        self.pd = ArrayPlotData()
+        self.pd.set_data('index', [])
+        self.pd.set_data('sindex', [])
+
+        self.pd.set_data('intensity', [])
+        self.pd.set_data('sintensity', [])
+
+        self.plot = Plot(data=self.pd)
+        self.plot.value_range.tight_bounds = False
+        self.plot.value_range.margin = 0.25
+        self.plot.plot(('index','intensity'), type='scatter', marker='circle', marker_size=1.5)
+        self.plot.plot(('sindex','sintensity'), color='green', line_width=1.5)
+
+    def traits_view(self):
+        v = View(UItem('plot', editor=ComponentEditor()))
+        return v
 
 
 class Controller(HasTraits):
@@ -69,7 +86,7 @@ class Controller(HasTraits):
 
     # The max number of data points to accumulate and show in the plot
     max_num_points = Int(100)
-    window = Float(10)
+    window = Float
     recording = Bool
     record_button = Button
     stop_button = Button
@@ -95,7 +112,7 @@ class Controller(HasTraits):
             self.device = dev
 
     def _window_changed(self):
-        self.max_num_points = int(self.window)
+        self.max_num_points = int(self.window*1000/float(PERIOD))
 
     def timer_tick(self, *args):
         """
@@ -112,14 +129,21 @@ class Controller(HasTraits):
         v = self.device.read_value()
         if v is None:
             return
-        # grab the existing data, truncate it, and append the new point.
-        # This isn't the most efficient thing in the world but it works.
-        cur_data = self.viewer.data
-        cur_index = self.viewer.index
-        new_data = hstack((cur_data[-self.max_num_points + 1:], [v]))
-        new_index = hstack((cur_index[-self.max_num_points + 1:], [ct]))
-        self.viewer.index = new_index
-        self.viewer.data = new_data
+
+        data = self.viewer.pd
+        index = data.get_data('index')
+        value1 = data.get_data('intensity')
+
+        value1 = hstack((value1[-self.max_num_points + 1:], [v]))
+        index = hstack((index[-self.max_num_points + 1:], [ct]))
+
+        data.set_data('intensity', value1)
+        data.set_data('index', index)
+
+        if len(index)>11:
+            y = smooth(value1)
+            data.set_data('sintensity', y)
+            data.set_data('sindex', index)
 
         if self._recording:
             with open(self._path, 'a') as rfile:
@@ -172,169 +196,11 @@ class LightSensorApplication(HasTraits):
     def _controller_default(self):
         ports = glob.glob('/dev/tty.usb*')
         return Controller(viewer=self.viewer,
-                          ports=ports + ['foo'])
+                          window=500,
+                          ports=ports)
 
 
 if __name__ == '__main__':
     app = LightSensorApplication()
     app.configure_traits()
 # ============= EOF =============================================
-# """
-# Visualization of simulated live data stream
-#
-# Shows how Chaco and Traits can be used to easily build a data
-# acquisition and visualization system.
-#
-# Two frames are opened: one has the plot and allows configuration of
-# various plot properties, and one which simulates controls for the hardware
-# device from which the data is being acquired; in this case, it is a mockup
-# random number generator whose mean and standard deviation can be controlled
-# by the user.
-# """
-#
-# # Major library imports
-# import numpy as np
-#
-# # Enthought imports
-# from traits.api import (Array, Callable, Enum, Float, HasTraits, Instance, Int,
-#                         Trait)
-# from traitsui.api import Group, HGroup, Item, View, spring, Handler
-# from pyface.timer.api import Timer
-#
-# # Chaco imports
-# from chaco.chaco_plot_editor import ChacoPlotItem
-#
-#
-# class Viewer(HasTraits):
-#     """ This class just contains the two data arrays that will be updated
-#     by the Controller.  The visualization/editor for this class is a
-#     Chaco plot.
-#     """
-#     index = Array
-#
-#     data = Array
-#
-#     plot_type = Enum("line", "scatter")
-#
-#     view = View(ChacoPlotItem("index", "data",
-#                               type_trait="plot_type",
-#                               resizable=True,
-#                               x_label="Time",
-#                               y_label="Signal",
-#                               color="blue",
-#                               bgcolor="white",
-#                               border_visible=True,
-#                               border_width=1,
-#                               padding_bg_color="lightgray",
-#                               width=800,
-#                               height=380,
-#                               marker_size=2,
-#                               show_label=False),
-#                 HGroup(spring, Item("plot_type", style='custom'), spring),
-#                 resizable = True,
-#                 buttons = ["OK"],
-#                 width=800, height=500)
-#
-#
-# class Controller(HasTraits):
-#
-#     # A reference to the plot viewer object
-#     viewer = Instance(Viewer)
-#
-#     # Some parameters controller the random signal that will be generated
-#     distribution_type = Enum("normal", "lognormal")
-#     mean = Float(0.0)
-#     stddev = Float(1.0)
-#
-#     # The max number of data points to accumulate and show in the plot
-#     max_num_points = Int(100)
-#
-#     # The number of data points we have received; we need to keep track of
-#     # this in order to generate the correct x axis data series.
-#     num_ticks = Int(0)
-#
-#     # private reference to the random number generator.  this syntax
-#     # just means that self._generator should be initialized to
-#     # random.normal, which is a random number function, and in the future
-#     # it can be set to any callable object.
-#     _generator = Trait(np.random.normal, Callable)
-#
-#     view = View(Group('distribution_type',
-#                       'mean',
-#                       'stddev',
-#                       'max_num_points',
-#                       orientation="vertical"),
-#                       buttons=["OK", "Cancel"])
-#
-#     def timer_tick(self, *args):
-#         """
-#         Callback function that should get called based on a timer tick.  This
-#         will generate a new random data point and set it on the `.data` array
-#         of our viewer object.
-#         """
-#         # Generate a new number and increment the tick count
-#         new_val = self._generator(self.mean, self.stddev)
-#         self.num_ticks += 1
-#
-#         # grab the existing data, truncate it, and append the new point.
-#         # This isn't the most efficient thing in the world but it works.
-#         cur_data = self.viewer.data
-#         new_data = np.hstack((cur_data[-self.max_num_points+1:], [new_val]))
-#         new_index = np.arange(self.num_ticks - len(new_data) + 1,
-#                               self.num_ticks + 0.01)
-#
-#         self.viewer.index = new_index
-#         self.viewer.data = new_data
-#         return
-#
-#     def _distribution_type_changed(self):
-#         # This listens for a change in the type of distribution to use.
-#         if self.distribution_type == "normal":
-#             self._generator = np.random.normal
-#         else:
-#             self._generator = np.random.lognormal
-#
-#
-# class DemoHandler(Handler):
-#
-#     def closed(self, info, is_ok):
-#         """ Handles a dialog-based user interface being closed by the user.
-#         Overridden here to stop the timer once the window is destroyed.
-#         """
-#
-#         info.object.timer.Stop()
-#         return
-#
-#
-# class Demo(HasTraits):
-#     controller = Instance(Controller)
-#     viewer = Instance(Viewer, ())
-#     timer = Instance(Timer)
-#     view = View(Item('controller', style='custom', show_label=False),
-#                 Item('viewer', style='custom', show_label=False),
-#                 handler=DemoHandler,
-#                 resizable=True)
-#
-#     def edit_traits(self, *args, **kws):
-#         # Start up the timer! We should do this only when the demo actually
-#         # starts and not when the demo object is created.
-#         self.timer=Timer(100, self.controller.timer_tick)
-#         return super(Demo, self).edit_traits(*args, **kws)
-#
-#     def configure_traits(self, *args, **kws):
-#         # Start up the timer! We should do this only when the demo actually
-#         # starts and not when the demo object is created.
-#         self.timer=Timer(100, self.controller.timer_tick)
-#         return super(Demo, self).configure_traits(*args, **kws)
-#
-#     def _controller_default(self):
-#         return Controller(viewer=self.viewer)
-#
-#
-# # NOTE: examples/demo/demo.py looks for a 'demo' or 'popup' or 'modal popup'
-# # keyword when it executes this file, and displays a view for it.
-# popup=Demo()
-#
-#
-# if __name__ == "__main__":
-#     popup.configure_traits()
